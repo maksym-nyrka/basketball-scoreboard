ec0bb7c1eaf7d43963e29373af926fd4e932a571
Maksym Nyrka <maksym.nyrka@gmail.com>
2023-01-14 14:01:33 +0200

    Add foul functionality

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let guestScoreEl = document.getElementById("guest-score")
 let homeScore = 0
 let guestScore = 0
 
-const TOTAL_GAME_SECONDS = 3
+const TOTAL_GAME_SECONDS = 12*60
 let currentSeconds = TOTAL_GAME_SECONDS
 
 let period = 1
@@ -65,22 +65,29 @@ function manageGame() {
 }
 
 function reset() {
+    resetTimer()
+    resetScores()
+    resetPeriod()
+    resetFouls()
+}
+
+function resetTimer() {
     currentSeconds = TOTAL_GAME_SECONDS
     startEl.textContent = "START GAME"
     timerEl.textContent = "00:00"
     clearInterval(timerInterval)
-
-    homeScore = 0
-    guestScore = 0
-    homeScoreEl.textContent = homeScore
-    guestScoreEl.textContent = guestScore
-
-    resetPeriod()
 }
 
 function resetPeriod() {
     period = 1
     periodInfoEl.textContent = "PERIOD " + period
+}
+
+function resetScores() {
+    homeScore = 0
+    guestScore = 0
+    homeScoreEl.textContent = homeScore
+    guestScoreEl.textContent = guestScore
 }
 
 function addHome(number) {
@@ -91,4 +98,64 @@ function addHome(number) {
 function addGuest(number) {
     guestScore += number
     guestScoreEl.textContent = guestScore
+}
+
+class Player {
+    PFCount = 0
+    TFCount = 0
+    docId
+
+    constructor(docId) {
+        this.docId = docId;
+      }
+
+    get docEl() {
+        return document.getElementById(this.docId)
+    }
+
+    addFoul(foulType) {
+        if(foulType == 'PF') {
+            this.PFCount++
+        } else if (foulType == 'TF') {
+            this.TFCount++
+        }
+    }
+
+    isPlayerOut() {
+        return (this.PFCount + this.TFCount) == 6 || this.TFCount == 2
+    }
+}
+
+let players = [new Player("player0-info"),
+                 new Player("player1-info"),
+                 new Player("player2-info"),
+                 new Player("player3-info"),
+                 new Player("player4-info"),
+                 new Player("player5-info"),
+                 new Player("player6-info"),
+                 new Player("player7-info"),
+                 new Player("player8-info"),
+                 new Player("player9-info")]
+
+function addFoul(playerIndex, foulType) {
+    let player = players[playerIndex]
+
+    if(player.isPlayerOut()) {
+        return
+    }
+    
+    player.addFoul(foulType)
+    player.docEl.textContent = " has " + player.PFCount + " PF" + ", " + player.TFCount + " TF"
+
+    if(player.isPlayerOut()) {
+        player.docEl.textContent = "Player is out."
+    }
+}
+
+function resetFouls() {
+    players.forEach((player) => {
+        player.PFCount = 0
+        player.TFCount = 0
+        player.docEl.textContent = ""
+    })
 }
